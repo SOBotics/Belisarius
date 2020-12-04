1c7ece1cea0ec75d0c79a3494b91b89801003da8
double-beep <doublebeep7@gmail.com>
2020-12-04 11:54:05 +0000

    Fix tests

--- src/test/java/bugs/stackoverflow/belisarius/services/PropertyServiceTest.java
+++ src/test/java/bugs/stackoverflow/belisarius/services/PropertyServiceTest.java
@@ -15,7 +15,7 @@ public class PropertyServiceTest {
         assertEquals("stackoverflow", propertyService.getProperty("site"));
         assertEquals("U4DMV*8nvpm3EOpvf69Rxw((", propertyService.getProperty("apikey"));
         assertEquals("true/false", propertyService.getProperty("useHiggs"));
-        assertEquals("https://api.higgs.sobotics.org", propertyService.getProperty("higgsurl"));
+        assertEquals("https://api.higgs.sobotics.org", propertyService.getProperty("higgsUrl"));
         assertEquals("3", propertyService.getProperty("higgsBotId"));
         assertEquals("false", propertyService.getProperty("outputMessage"));
         assertEquals("true", propertyService.getProperty("useRedunda"));
